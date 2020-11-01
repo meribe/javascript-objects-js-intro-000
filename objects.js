6c9cb6116eea50d374f0fd6847ca9f43d1d21b55
witty-abstraction-1855 <merve.kastan4@gmail.com>
2020-11-01 18:20:20 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist= {edsheeran:"Shape of you", justin timberlake: "mirrors"};
+function updatePlaylist (playlist,artist name, song title ){
+  return Object.assign({},playlist,{[artistName]: songTitle})
+}
+
+function removeFromPlaylist(playlistObject,artistName) {
+delete.playlist.edsheeran;
+return playlist
+}
